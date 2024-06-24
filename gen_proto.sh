be38945866749de3988dc5f635582acd93ef8128
user <user@example.com>
2024-06-24 14:41:54 +0300

    add tool

--- gen_proto.sh
+++ gen_proto.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative pkg/proto/keeper.proto
 
-protoc --go_out=. --go_opt=paths=source_relative \
-  --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-  --grpc-gateway_out=. --grpc-gateway_opt=paths=source_relative \
-  --openapiv2_out=. --openapiv2_opt=use_go_templates=true \
-  pkg/proto/keeper.proto
+export GOPATH=$HOME/go;
+export GO_LIBS=$HOME/go_libs;
+export PATH=$PATH:$GOPATH/bin;
+export PATH=$PATH:$GO_LIBS/bin;
+protoc -I ./protobuf \
+  --go_out=./pkg/proto --go_opt=paths=source_relative \
+  --go-grpc_out=./pkg/proto --go-grpc_opt=paths=source_relative \
+  --grpc-gateway_out=./pkg/proto --grpc-gateway_opt=paths=source_relative \
+  --openapiv2_out=./swagger-ui --openapiv2_opt=use_go_templates=true \
+  protobuf/keeper.proto
