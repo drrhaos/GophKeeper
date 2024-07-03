7c15d831e31502ff6d4237ccf92781e4defbd208
user <user@example.com>
2024-07-03 21:53:28 +0300

    add crypt test

--- internal/crypt/crypt_test.go
+++ internal/crypt/crypt_test.go
@@ -2,14 +2,14 @@
 package crypt
 
 import (
-	"fmt"
 	"gophkeeper/pkg/proto"
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEncField(t *testing.T) {
-
 	type args struct {
 		dec       *proto.FieldKeep
 		secretKey string
@@ -25,7 +25,7 @@ func TestEncField(t *testing.T) {
 				dec: &proto.FieldKeep{
 					Name:     "Test",
 					Login:    "Test",
-					Password: "Test",
+					Password: "",
 				},
 				secretKey: "akjdsfakjLKJNaskdflkjnJnlasjkdnf",
 			},
@@ -34,10 +34,41 @@ func TestEncField(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotEnc := EncField(tt.args.dec, tt.args.secretKey); !reflect.DeepEqual(gotEnc, tt.wantEnc) {
-				decc := DecField(gotEnc, tt.args.secretKey)
-				fmt.Println(decc.Login)
-				fmt.Println(decc.Login)
-				// t.Errorf("EncField() = %v, want %v", gotEnc, tt.wantEnc)
+				decr := DecField(gotEnc, tt.args.secretKey)
+				assert.Equal(t, tt.args.dec.Name, decr.Name)
+
+			}
+		})
+	}
+}
+
+func TestDecField(t *testing.T) {
+	type args struct {
+		enc       *proto.FieldKeep
+		secretKey string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantDec *proto.FieldKeep
+	}{
+		{
+			name: "positive",
+			args: args{
+				enc: &proto.FieldKeep{
+					Name:     "OXxQERV0nvMhHmW8GmQV37CBQsXCI6ux928mlw==",
+					Login:    "OXxQERV0nvMhHmW8GmQV37CBQsXCI6ux928mlw==",
+					Password: "OXxQERV0nvMhHmW8GmQV37CBQsXCI6ux928mlw==",
+				},
+				secretKey: "akjdsfakjLKJNaskdflkjnJnlasjkdnf",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotDec := DecField(tt.args.enc, tt.args.secretKey); !reflect.DeepEqual(gotDec, tt.wantDec) {
+				assert.Equal(t, "", gotDec.Name)
+
 			}
 		})
 	}
