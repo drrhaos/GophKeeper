c029d44a65ecee3ee952807b769819bc9c95d4b4
user <user@example.com>
2024-07-13 17:25:30 +0300

    add test file

---
+++ internal/server/grpcmode/file_test.go
@@ -0,0 +1,76 @@
+package grpcmode
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestFile_SetFile(t *testing.T) {
+	fileTmp := "/tmp/agent.json"
+	defer os.Remove(fileTmp)
+	dirPath, nameFile := filepath.Split(fileTmp)
+
+	type args struct {
+		fileName string
+		path     string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Positive test #1",
+			args: args{
+				fileName: nameFile,
+				path:     dirPath,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := NewFile()
+			if err := f.SetFile(tt.args.fileName, tt.args.path); (err != nil) != tt.wantErr {
+				t.Errorf("File.SetFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestFile_Write(t *testing.T) {
+	fileTmp := "/tmp/agent.json"
+	defer os.Remove(fileTmp)
+	dirPath, nameFile := filepath.Split(fileTmp)
+
+	type args struct {
+		fileName string
+		path     string
+		chunk    []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Positive test #1",
+			args: args{
+				fileName: nameFile,
+				path:     dirPath,
+				chunk:    []byte("test"),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := NewFile()
+			f.SetFile(tt.args.fileName, tt.args.path)
+			if err := f.Write(tt.args.chunk); (err != nil) != tt.wantErr {
+				t.Errorf("File.Write() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
